b55591e0379f636dd352fc1a4f6f384d9939df11
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-11-11 17:35:00 -0500

    Filling in more for the libservice

--- library-service/src/main/java/com/example/libraryservice/controller/ProductsPrices.java
+++ library-service/src/main/java/com/example/libraryservice/controller/ProductsPrices.java
@@ -5,20 +5,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProductsPrices {
 
-	
-	public long getBooksPrice() {
-		return booksPrice;
-	}
-	public void setBooksPrice(long booksPrice) {
-		this.booksPrice = booksPrice;
-	}
-	public long getCoursesPrice() {
-		return coursesPrice;
-	}
-	public void setCoursesPrice(long coursesPrice) {
-		this.coursesPrice = coursesPrice;
-	}
-	long booksPrice;
-	long  coursesPrice;
-	
+
+    long booksPrice;
+    long coursesPrice;
+
+    public long getBooksPrice() {
+        return booksPrice;
+    }
+
+    public void setBooksPrice(long booksPrice) {
+        this.booksPrice = booksPrice;
+    }
+
+    public long getCoursesPrice() {
+        return coursesPrice;
+    }
+
+    public void setCoursesPrice(long coursesPrice) {
+        this.coursesPrice = coursesPrice;
+    }
+
 }
