5f2d5c0330e2f62c38f8d2cfc6f7fbd6c26098b5
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-01-16 22:04:09 -0500

    Get. PactProviderTest to work. Tweaking Consumer contract test temporarily disabling some pieces.

--- Courses/src/test/java/com/example/demo/PactProviderTest.java
+++ Courses/src/test/java/com/example/demo/PactProviderTest.java
@@ -1,15 +1,53 @@
 package com.example.demo;
 
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.StateChangeAction;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Provider("CoursesCatalogue")
+@PactFolder("pacts")
 public class PactProviderTest {
+    @LocalServerPort
+    public int port;
+
     @TestTemplate
-    public void pactVerificationTest(PactVerificationContext context)  {
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    public void pactVerificationTest(PactVerificationContext context) {
         context.verifyInteraction();
+    }
+
+    @BeforeEach
+    public void setup(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", port));
+    }
+
+    @State(value = "courses exist", action = StateChangeAction.SETUP)
+    public void coursesExist() {
+
+    }
+
+    @State(value = "courses exist", action = StateChangeAction.TEARDOWN)
+    public void coursesExistTearDown() {
+
+    }
+
+    @State(value = "Course Appium exist", action = StateChangeAction.SETUP)
+    public void coursesAppiumExist() {
+
+    }
+
+    @State(value = "Course Appium exist", action = StateChangeAction.TEARDOWN)
+    public void ccoursesAppiumTearDown() {
+
     }
 }
